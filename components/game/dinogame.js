1a43b257077e3325d93eeab37773ea05739e8d64
Lewis Jelfs <97512035+ljelfswebdev@users.noreply.github.com>
2022-04-13 15:41:15 +0200

    defence added

--- components/game/dinogame.js
+++ components/game/dinogame.js
@@ -5,14 +5,14 @@ import Dinodex from './dinodex'
 class Dinogame extends Component {
     static defaultProps = {
         dinosaur : [
-            {id: 1, name: 'Allosaurus', type: 'carnivorous', base_experience: 87, imageUrl:'./images/dinosaurs/allosaurus.jpg'},
-            {id: 2, name: 'Brachiosaurus', type: 'herbivorous', base_experience: 63, imageUrl:'./images/dinosaurs/Brachiosaurus.jpg'},
-            {id: 3, name: 'Spinosaurus', type: 'carnivorous', base_experience: 130, imageUrl:'./images/dinosaurs/spinosaurus.jpeg'},
-            {id: 4, name: 'Tyrannosaurus Rex', type: 'carnivorous', base_experience: 178, imageUrl:'./images/dinosaurs/trex.jpeg'},
-            {id: 5, name: 'Velociraptor', type: 'carnivorous', base_experience: 162, imageUrl:'./images/dinosaurs/velociraptor.jpeg'},
-            {id: 6, name: 'Triceratops', type: 'herbivorous', base_experience: 155, imageUrl:'./images/dinosaurs/triceratops.jpg'},
-            {id: 7, name: 'Diplodocus', type: 'herbivorous', base_experience: 60, imageUrl:'./images/dinosaurs/diplodocus.jpg'},
-            {id: 8, name: 'Pterodactyl', type: 'carnivorous', base_experience: 101, imageUrl:'./images/dinosaurs/pterodactyl.png'}
+            {id: 1, name: 'Allosaurus', type: 'carnivorous', base_experience: 87, base_defence: 84, imageUrl:'./images/dinosaurs/allosaurus.jpg'},
+            {id: 2, name: 'Brachiosaurus', type: 'herbivorous', base_experience: 63, base_defence: 180, imageUrl:'./images/dinosaurs/Brachiosaurus.jpg'},
+            {id: 3, name: 'Spinosaurus', type: 'carnivorous', base_experience: 130, base_defence: 94, imageUrl:'./images/dinosaurs/spinosaurus.jpeg'},
+            {id: 4, name: 'Tyrannosaurus Rex', type: 'carnivorous', base_experience: 178, base_defence: 166, imageUrl:'./images/dinosaurs/trex.jpeg'},
+            {id: 5, name: 'Velociraptor', type: 'carnivorous', base_experience: 162, base_defence: 64, imageUrl:'./images/dinosaurs/velociraptor.jpeg'},
+            {id: 6, name: 'Triceratops', type: 'herbivorous', base_experience: 155, base_defence: 160, imageUrl:'./images/dinosaurs/triceratops.jpg'},
+            {id: 7, name: 'Diplodocus', type: 'herbivorous', base_experience: 60, base_defence: 140, imageUrl:'./images/dinosaurs/diplodocus.jpg'},
+            {id: 8, name: 'Pterodactyl', type: 'carnivorous', base_experience: 101, base_defence: 60, imageUrl:'./images/dinosaurs/pterodactyl.png'}
         ]
     };
     render() {
@@ -23,8 +23,8 @@ class Dinogame extends Component {
             let randDinosaur = hand2.splice(randIdx, 1)[0];
             hand1.push(randDinosaur);
         }
-        let exp1 = hand1.reduce((exp, dinosaur) => exp+dinosaur.base_experience, 0);
-        let exp2 = hand2.reduce((exp, dinosaur) => exp+dinosaur.base_experience, 0);
+        let exp1 = hand1.reduce((exp, dinosaur) => exp+dinosaur.base_experience+dinosaur.base_defence, 0);
+        let exp2 = hand2.reduce((exp, dinosaur) => exp+dinosaur.base_experience+dinosaur.base_defence, 0);
         return (
             <div>
                 <Dinodex dinosaur={hand1} exp={exp1} isWinner={exp1 > exp2} />
